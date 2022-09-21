123a68a97ff26101152f2ceae3e17eb67ac69860
Yilkal Gebeyehu Mekonnen <Yilkalg3@gmail.com>
2022-09-21 17:17:05 +0300

    Update CART.py

--- python-api/CART.py
+++ python-api/CART.py
@@ -5,3 +5,5 @@ Map = geemap.Map(center=(10.41, 37.62), zoom=11)
 
 jedeb = ee.FeatureCollection("projects/ee-yilkalgebeyehu/assets/Jedeb_Watershed")
 #Map.addLayer(jedeb, {}, 'Jedeb Watershed')
+
+# Set Dates (Based on Planting and Harvesting Date)
