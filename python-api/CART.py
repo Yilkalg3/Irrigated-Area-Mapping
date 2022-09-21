0b6d46b7d885e9661cd518d042a77cd2399397c1
Yilkal Gebeyehu Mekonnen <Yilkalg3@gmail.com>
2022-09-21 17:16:08 +0300

    Update CART.py

--- python-api/CART.py
+++ python-api/CART.py
@@ -1,2 +1,7 @@
 import ee, geemap,time
 
+# Intialize geemap
+Map = geemap.Map(center=(10.41, 37.62), zoom=11)
+
+jedeb = ee.FeatureCollection("projects/ee-yilkalgebeyehu/assets/Jedeb_Watershed")
+#Map.addLayer(jedeb, {}, 'Jedeb Watershed')
