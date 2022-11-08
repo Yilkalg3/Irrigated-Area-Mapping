2725eb9b5b7d3b72505a2c18404ccfba8adc554d
Yilkal Gebeyehu Mekonnen <Yilkalg3@gmail.com>
2022-11-08 14:51:28 +0300

    Delete Gradient_Tree_Boost.js

--- javascript/Gradient_Tree_Boost.js
+++
@@ -1,7 +0,0 @@
-// Import Study area geometry and GCPs
-
-var jedeb = ee.FeatureCollection("projects/ee-yilkalgebeyehu/assets/Jedeb_Watershed");
-var trainingGcp = ee.FeatureCollection("projects/ee-yilkalgebeyehu/assets/Training");
-var validationGcp = ee.FeatureCollection("projects/ee-yilkalgebeyehu/assets/Validation");
-
-
